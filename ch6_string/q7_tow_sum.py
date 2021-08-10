d5e41a4b500e5572b82b5d1b42de45a2f22f8822
서명석(Myeongseok Seo) 과장 낙스넷 <myeongseok1.seo@doosan.com>
2021-08-10 20:00:22 +0900

    7장 배열 08 빗물 트래핑

--- ch6_string/q7_tow_sum.py
+++ ch6_string/q7_tow_sum.py
@@ -3,15 +3,19 @@
 두수의 합
 덧셈하여 타겟을 만들 수 있는 배열의 두 숫자 인덱스를 리턴하라.
 """
-from _ast import List
+from typing import List
 
 
+
+nums = [2, 7, 11, 15]
+target = 9
 # 브루트 포스로 계산
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
+                    print([i, j])
                     return [i, j]
 
 
@@ -51,4 +55,22 @@ class Solution3:
 
 
 # 투 포인터 이용
+class Solution4:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # sort할 경우 인덱스 문제 발생
+        # nums.sort()
+        left, right = 0, len(nums) - 1
+        while not left == right:
+            # 합이 타겟보다 작으면 왼쪽 포인터를 오른쪽으로
+            if nums[left] + nums[right] < target:
+                left += 1
+            # 합이 타겟보다 크면 오른쪽 포인터를 왼쪽으로
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                print([left, right])
+                return [left, right]
 
+
+s = Solution4()
+s. twoSum(nums, target)
