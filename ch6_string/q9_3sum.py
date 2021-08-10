5861afa8dc13f114b8fbe34bb8166902b944506c
서명석(Myeongseok Seo) 과장 낙스넷 <myeongseok1.seo@doosan.com>
2021-08-10 21:52:35 +0900

    7장 배열 09 세수의 합 - 브루트 포스

---
+++ ch6_string/q9_3sum.py
@@ -0,0 +1,40 @@
+
+
+"""
+세수의 합
+배열을 입력받아 합으로 0을 만들 수 있는 3개의 엘리먼트를 출력하라
+"""
+from typing import List
+
+nums = [-1, 0, 1, 2, -1, -4]
+output = [
+    [-1, 0, 1],
+    [-1, -1, 2]
+]
+
+# 브루트 포스로 계산
+# leetcode timeout 발생 시간복잡도 O(n^3)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        nums.sort()
+
+        # 브루트 포스 n^3 반복
+        for i in range(len(nums) - 2):
+            # 중복된 값 건너뛰기
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 1):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                for k in range(j + 1, len(nums)):
+                    if k > j + 1 and nums[k] == nums[k - 1]:
+                        continue
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        results.append([nums[i], nums[j], nums[k]])
+        print(results)
+        return results
+
+
+s = Solution()
+s.threeSum(nums)
